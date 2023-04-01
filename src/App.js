c42575603f769c0d2e4ce569083a35cd4c2f5824
José Antonio Trejo Gutiérrez <jose.trejog@sat.gob.mx>
2023-03-31 20:18:28 -0600

    Styles were added and a container with the logo was created.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import freeCodeCampLogo from './images/free_code_camp_logo.jpg'
 
 function App() {
   return (
     <div className="App">
+      <div className='freecodecamp_logo_container'>
+        <img
+          className='freecodecomp_logo'
+          src={freeCodeCampLogo}
+          alt='Freecodecamp logo'
+        />
+      </div>
+      <div className='main-container'>
+
+      </div>
       
     </div>
   );
